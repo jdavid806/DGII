375057331c998c9eb21fa8297c88ad2a8f926dcc
Jeffer <bogotadiscoverapp@gmail.com>
2025-07-31 17:36:02 -0500

    resolve: conflictos de archivos auto-generados

--- src/main/java/com/medicalsoft/infrastructure/dgii/generated/ecf31/TipoMonedaType.java
+++ src/main/java/com/medicalsoft/infrastructure/dgii/generated/ecf31/TipoMonedaType.java
@@ -2,6 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2025.07.31 a las 05:27:04 PM COT 
 // Generado el: 2025.07.31 a las 09:17:09 AM COT 
 //
 
