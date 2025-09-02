2c1563c7990a4607ee4586ce8a2ca5cb1765305f
jdavid806 <bogotadiscoverapp@gmail.com>
2025-09-02 14:04:02 -0500

    fix
    fix

--- src/main/java/com/medicalsoft/infrastructure/dgii/generated/ecf32/ECF.java
+++ src/main/java/com/medicalsoft/infrastructure/dgii/generated/ecf32/ECF.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2025.09.02 a las 10:50:17 AM COT 
+// Generado el: 2025.09.02 a las 01:46:46 PM COT 
 //
 
 
